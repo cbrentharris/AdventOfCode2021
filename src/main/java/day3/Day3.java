1922ff4dd1f5d81691a9492cc065489ac0868a15
Christopher Harris <chharris@chharris-mn2.linkedin.biz>
2021-12-03 00:28:46 -0600

    Day3 Done

--- src/main/java/day3/Day3.java
+++ src/main/java/day3/Day3.java
@@ -1,6 +1,87 @@
 package day3;
 
+import java.util.BitSet;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Day3 {
 
-  public void todo() {}
+  public static int powerConsuption(List<BitSet> bitSets) {
+    if (bitSets.size() == 0) {
+      return 0;
+    }
+
+    int numBits = 12;
+    BitSet gammaBits = new BitSet(numBits);
+    BitSet epsilonBits = new BitSet(numBits);
+    for (int i = 0; i < numBits; i++) {
+      Map<Boolean, Long> frequencies = getBitFrequencies(bitSets, i);
+
+      boolean largestBit = frequencies.getOrDefault(true, 0L) > frequencies.getOrDefault(false, 0L);
+
+      gammaBits.set(i, largestBit);
+      epsilonBits.set(i, !largestBit);
+    }
+    int gamma = toInt(gammaBits, numBits);
+    int epsilon = toInt(epsilonBits, numBits);
+    return gamma * epsilon;
+  }
+
+  public static int lifeSupport(List<BitSet> bitSets, int numBits) {
+    Set<BitSet> oxygenRatings = new HashSet<>(bitSets);
+    Set<BitSet> co2ScrubberRatings = new HashSet<>(bitSets);
+    for (int i = 0; i < numBits; i++) {
+      boolean oxygenBit = mostCommonBit(oxygenRatings, i);
+      boolean co2Bit = leastCommonBit(co2ScrubberRatings, i);
+      oxygenRatings = filter(i, oxygenRatings, oxygenBit);
+      co2ScrubberRatings = filter(i, co2ScrubberRatings, co2Bit);
+    }
+    int oxygenScrubberRating = toInt(oxygenRatings.iterator().next(), numBits);
+    int co2ScrubberRating = toInt(co2ScrubberRatings.iterator().next(), numBits);
+    return oxygenScrubberRating * co2ScrubberRating;
+  }
+
+  private static Set<BitSet> filter(int i, Set<BitSet> ratings, boolean valueToKeep) {
+    if (ratings.size() == 1) {
+      return ratings;
+    }
+    return ratings.stream().filter(b -> b.get(i) == valueToKeep).collect(Collectors.toSet());
+  }
+
+  /** Manual conversion of an bit set to an int, unsigned with a max number of bits */
+  private static int toInt(BitSet b, int numBits) {
+    int res = 0;
+    for (int i = numBits - 1; i >= 0; i--) {
+      if (b.get(i)) {
+        res = res + ((Double) Math.pow(2, numBits - 1 - i)).intValue();
+      }
+    }
+    return res;
+  }
+
+  /** How often a 0 or 1 occurs in a list of bit sets */
+  private static Map<Boolean, Long> getBitFrequencies(Collection<BitSet> bitSets, int bitIndex) {
+    return bitSets.stream()
+        .map(bitSet -> bitSet.get(bitIndex))
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+  }
+
+  /** Find the least common bit for a given index in a list of bit sets. Tie goes to 0 */
+  private static boolean leastCommonBit(Collection<BitSet> bitSets, int bitIndex) {
+    Map<Boolean, Long> frequencies = getBitFrequencies(bitSets, bitIndex);
+    long falseFrequency = frequencies.getOrDefault(false, 0L);
+    long trueFrequency = frequencies.getOrDefault(true, 0L);
+    return !(falseFrequency <= trueFrequency);
+  }
+
+  /** Find the most common bit for a given index in a list of bit sets. Tie goes to 1 */
+  private static boolean mostCommonBit(Collection<BitSet> bitSets, int bitIndex) {
+    Map<Boolean, Long> frequencies = getBitFrequencies(bitSets, bitIndex);
+    return frequencies.getOrDefault(true, 0L) >= frequencies.getOrDefault(false, 0L);
+  }
 }
--- src/test/java/Day3Test.java
+++ src/test/java/Day3Test.java
@@ -1,5 +1,7 @@
 import static org.assertj.core.api.Assertions.*;
 
+import day3.Day3;
+import java.util.BitSet;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -9,15 +11,38 @@ public class Day3Test extends AbstractAdventTest {
 
   @Test
   public void testDay3Part1() {
-    assertThat(getString()).hasSize(1);
+    assertThat(Day3.powerConsuption(getBitSets())).isEqualTo(3912944);
   }
 
   @Test
   public void testDay3Part2() {
-    assertThat(getString()).hasSize(1);
+    assertThat(Day3.lifeSupport(getBitSets(), 12)).isEqualTo(4996233);
   }
 
-  private List<String> getString() {
-    return Stream.of(getData("./day3.txt").split("\n")).collect(Collectors.toList());
+  @Test
+  public void testDay3Part2TestInput() {
+    assertThat(Day3.lifeSupport(getBitSets("./day3-test.txt"), 5)).isEqualTo(230);
+  }
+
+  private List<BitSet> getBitSets() {
+    return getBitSets("./day3.txt");
+  }
+
+  private List<BitSet> getBitSets(String fileName) {
+    return Stream.of(getData(fileName).split("\n"))
+        .map(
+            s -> {
+              String[] parts = s.split("");
+              BitSet bitSet = new BitSet(parts.length);
+              for (int i = 0; i < parts.length; i++) {
+                if (parts[i].equals("1")) {
+                  bitSet.set(i, true);
+                } else {
+                  bitSet.set(i, false);
+                }
+              }
+              return bitSet;
+            })
+        .collect(Collectors.toList());
   }
 }
