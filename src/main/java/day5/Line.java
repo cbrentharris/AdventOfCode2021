efd0fcd8887cba618f2cd9bee45245a4ae515330
Christopher Harris <chharris@chharris-mn2.linkedin.biz>
2021-12-05 11:45:42 -0600

    Day 5 done

---
+++ src/main/java/day5/Line.java
@@ -0,0 +1,63 @@
+package day5;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class Line {
+  @Data
+  private static class Slope {
+    private int xPart;
+    private int yPart;
+
+    public Slope(Coordinate start, Coordinate end) {
+      int rawXPart = end.getX() - start.getX();
+      int rawYPart = end.getY() - start.getY();
+
+      int gcd = gcd(rawYPart, rawYPart);
+      if (gcd == 0) {
+        gcd = 1;
+      }
+
+      xPart = rawXPart / gcd;
+      yPart = rawYPart / gcd;
+
+      if (xPart == 0 && yPart != 0) {
+        yPart = (yPart / gcd(yPart, yPart));
+      } else if (xPart != 0 && yPart == 0) {
+        xPart = (xPart / gcd(xPart, xPart));
+      }
+    }
+
+    private static int gcd(int a, int b) {
+      if (a == 0) {
+        return Math.abs(b);
+      }
+      return gcd(b % a, a);
+    }
+
+    public Coordinate advance(Coordinate coordinate) {
+      return new Coordinate(coordinate.getX() + xPart, coordinate.getY() + yPart);
+    }
+  }
+
+  private final Coordinate start;
+  private final Coordinate end;
+
+  public List<Coordinate> getCrossedCoordinates() {
+    List<Coordinate> output = new ArrayList<>();
+    Coordinate current = new Coordinate(start.getX(), start.getY());
+    Slope slope = new Slope(start, end);
+    output.add(current);
+    while (!current.equals(end)) {
+      current = slope.advance(current);
+      output.add(current);
+    }
+    return output;
+  }
+
+  public boolean isStraightLine() {
+    return start.getX() == end.getX() || start.getY() == end.getY();
+  }
+}
---
+++ src/test/java/Day5Test.java
@@ -0,0 +1,47 @@
+import static org.assertj.core.api.Assertions.*;
+
+import day5.Coordinate;
+import day5.Day5;
+import day5.Line;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+
+public class Day5Test extends AbstractAdventTest {
+  @Test
+  public void day5Part1Test() {
+    assertThat(Day5.overlappingStraightLines(getLines("./day5-test.txt"))).isEqualTo(5);
+  }
+
+  @Test
+  public void day5Part1() {
+    assertThat(Day5.overlappingStraightLines(getLines("./day5.txt"))).isEqualTo(4745);
+  }
+
+  @Test
+  public void day5Part2Test() {
+    assertThat(Day5.overlappingLines(getLines("./day5-test.txt"))).isEqualTo(12);
+  }
+
+  @Test
+  public void day5Part2() {
+    assertThat(Day5.overlappingLines(getLines("./day5.txt"))).isEqualTo(18442);
+  }
+
+  private List<Line> getLines(String fileName) {
+    return Stream.of(getData(fileName).split("\n"))
+        .map(
+            rawLine -> {
+              String[] parts = rawLine.split(" -> ");
+              String[] rawStart = parts[0].split(",");
+              String[] rawEnd = parts[1].split(",");
+              Coordinate start =
+                  new Coordinate(Integer.parseInt(rawStart[0]), Integer.parseInt(rawStart[1]));
+              Coordinate end =
+                  new Coordinate(Integer.parseInt(rawEnd[0]), Integer.parseInt(rawEnd[1]));
+              return new Line(start, end);
+            })
+        .collect(Collectors.toList());
+  }
+}
